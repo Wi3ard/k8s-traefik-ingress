fe1ff66537ecbc8225ea944f49aa0452335dbd4d
Alex Kucher <a.kucher@avantize.com>
2019-01-14 18:55:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# k8s-traefik-ingress
+# Traefik ingress controller for Kubernetes
+
+- [Traefik ingress controller for Kubernetes](#traefik-ingress-controller-for-kubernetes)
+  - [Terraform initialization](#terraform-initialization)
+  - [Apply Terraform plan](#apply-terraform-plan)
+  - [Managing persistent volumes](#managing-persistent-volumes)
+    - [Change reclaim policy for application volumes](#change-reclaim-policy-for-application-volumes)
+    - [Delete persistent volumes](#delete-persistent-volumes)
+
 Terraform configuration for deploying Traefik ingress controller in a Kubernetes cluster
+
+## Terraform initialization
+
+Copy [terraform.tfvars.example](terraform.tfvars.example) file to `terraform.tfvars` and set input variables values as per your needs. Then initialize Terraform with `init` command:
+
+```shell
+terraform init -backend-config "bucket=$BUCKET_NAME" -backend-config "prefix=apps/traefik" -backend-config "region=$REGION"
+```
+
+- `$REGION` should be replaced with a region name.
+- `$BUCKET_NAME` should be replaced with a GCS Terraform state storage bucket name.
+
+## Apply Terraform plan
+
+To apply Terraform plan, run:
+
+```shell
+terraform apply
+```
+
+## Managing persistent volumes
+
+Some of the included services require persistent storage, configured through Persistent Volumes that specify which disks your cluster has access to.
+
+Storage changes after installation need to be manually handled by your cluster administrators. Automated management of these volumes after installation is not handled by the deployment scripts.
+
+> **IMPORTANT**: you may experience a total data loss if these changes are not applied properly. Specifically, if you don't change the default `Delete` [reclaimPolicy](https://kubernetes.io/docs/concepts/storage/storage-classes/#reclaim-policy) for [PersistentVolumes](https://kubernetes.io/docs/concepts/storage/persistent-volumes/#persistent-volumes) to `Retain`, the inderlying [Google Persistent Disk](https://cloud.google.com/compute/docs/disks/) will be completely destroyed by GCE upon destruction of a Kubernetes application stack.
+
+### Change reclaim policy for application volumes
+
+Find the volumes/claims that are being used, and change the `reclaimPolicy` for each from `Delete` to `Retain`:
+
+```shell
+$ kubectl get pv -n kube-system | grep datadir-consul
+pvc-d4184653-179a-11e9-ad5f-42010a80029a   1Gi        RWO            Delete           Bound     kube-system/datadir-consul-0   standard                 9h
+
+$ kubectl patch pv pvc-d4184653-179a-11e9-ad5f-42010a80029a -p "{\"spec\":{\"persistentVolumeReclaimPolicy\":\"Retain\"}}"
+persistentvolume "pvc-d4184653-179a-11e9-ad5f-42010a80029a" patched
+
+$ kubectl get pv -n kube-system | grep datadir-consul
+pvc-d4184653-179a-11e9-ad5f-42010a80029a   1Gi        RWO            Retain           Bound     kube-system/datadir-consul-0   standard                 9h
+```
+
+### Delete persistent volumes
+
+After you uninstall this plan from the cluster, and **you are completely sure** you don't need its persisten volumes anymore, you can delete them using following commands:
+
+```shell
+$ kubectl get pvc -n kube-system | grep datadir-consul
+datadir-consul-0   Bound     pvc-d4184653-179a-11e9-ad5f-42010a80029a   1Gi        RWO            standard       15h
+
+$ kubectl delete pvc datadir-consul-0 -n kube-system
+persistentvolumeclaim "datadir-consul-0" deleted
+
+$ gcloud compute disks list --filter="-users:*"
+NAME                                                             ZONE           SIZE_GB  TYPE         STATUS
+gke-dev-a3f54e52-dynam-pvc-e41dcd6c-179a-11e9-ad5f-42010a80029a  us-central1-a  1        pd-standard  READY
+
+$ gcloud compute disks delete gke-dev-a3f54e52-dynam-pvc-e41dcd6c-179a-11e9-ad5f-42010a80029a --zone=us-central1-a
+The following disks will be deleted:
+ - [gke-dev-a3f54e52-dynam-pvc-e41dcd6c-179a-11e9-ad5f-42010a80029a]
+in [us-central1-a]
+
+Do you want to continue (Y/n)?  y
+
+Deleted [https://www.googleapis.com/compute/v1/projects/mtm-default-1/zones/us-central1-a/disks/gke-dev-a3f54e52-dynam-pvc-e41dcd6c-179a-11e9-ad5f-42010a80029a].
+```
---
+++ main.tf
@@ -0,0 +1,201 @@
+/*
+ * Input variables.
+ */
+
+variable "acme_email" {
+  description = "Admin e-mail for Let's Encrypt"
+  type        = "string"
+}
+
+variable "domain_name" {
+  description = "Root domain name for the stack"
+  type        = "string"
+}
+
+variable "dns_zone_name" {
+  description = "The unique name of the zone hosted by Google Cloud DNS"
+  type        = "string"
+}
+
+variable "google_project_id" {
+  description = "GCE project ID"
+  type        = "string"
+}
+
+variable "region" {
+  default     = "us-central1"
+  description = "Region to create resources in"
+  type        = "string"
+}
+
+/*
+ * Terraform providers.
+ */
+
+provider "google" {
+  version = "~> 1.20"
+
+  project = "${var.google_project_id}"
+  region  = "${var.region}"
+}
+
+provider "helm" {
+  version = "~> 0.7"
+}
+
+provider "kubernetes" {
+  version = "~> 1.4"
+}
+
+/*
+ * GCS remote storage for storing Terraform state.
+ */
+
+terraform {
+  backend "gcs" {}
+}
+
+/*
+ * Terraform resources.
+ */
+
+# Consul helm chart.
+resource "helm_release" "consul" {
+  chart         = "stable/consul"
+  force_update  = true
+  name          = "consul"
+  namespace     = "kube-system"
+  recreate_pods = true
+  reuse_values  = true
+
+  values = [<<EOF
+ImageTag: "1.4.0"
+EOF
+  ]
+}
+
+# Traefik helm chart.
+resource "helm_release" "traefik" {
+  depends_on = ["helm_release.consul"]
+
+  chart         = "stable/traefik"
+  force_update  = true
+  name          = "traefik"
+  namespace     = "kube-system"
+  recreate_pods = true
+  reuse_values  = true
+
+  values = [<<EOF
+accessLogs:
+  enabled: false
+acme:
+  challengeType: "http-01"
+  email: "${var.acme_email}"
+  enabled: true
+  logging: true
+  onHostRule: true
+  persistence:
+    enabled: false
+  staging: false
+forwardedHeaders:
+  enabled: true
+  trustedIPs:
+    - 0.0.0.0/0
+kvprovider:
+  storeAcme: true
+  consul:
+    endpoint: "consul:8500"
+    prefix: traefik
+    watch: true
+rbac:
+  enabled: true
+ssl:
+  cipherSuites: [
+    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
+    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
+    "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+  ]
+  enabled: true
+  enforced: true
+  tlsMinVersion: VersionTLS12
+cpuRequest: 200m
+memoryRequest: 100Mi
+cpuLimit: 400m
+memoryLimit: 300Mi
+EOF
+  ]
+}
+
+# Traefik autoscaler.
+resource "kubernetes_horizontal_pod_autoscaler" "traefik" {
+  metadata {
+    name      = "${helm_release.traefik.metadata.0.name}"
+    namespace = "${helm_release.traefik.metadata.0.namespace}"
+
+    labels {
+      app     = "traefik"
+      release = "traefik"
+    }
+  }
+
+  spec {
+    max_replicas                      = 10
+    min_replicas                      = 2
+    target_cpu_utilization_percentage = 50
+
+    scale_target_ref {
+      api_version = "extensions/v1beta1"
+      kind        = "Deployment"
+      name        = "${helm_release.traefik.name}"
+    }
+  }
+}
+
+# Data source for Traefik Helm chart.
+data "kubernetes_service" "traefik" {
+  depends_on = ["helm_release.traefik"]
+
+  metadata {
+    name      = "${helm_release.traefik.metadata.0.name}"
+    namespace = "${helm_release.traefik.metadata.0.namespace}"
+  }
+}
+
+# DNS zone managed by Google Cloud DNS.
+data "google_dns_managed_zone" "default" {
+  name = "${var.dns_zone_name}"
+}
+
+# Root A record.
+resource "google_dns_record_set" "a_root" {
+  name         = "${var.domain_name}."
+  managed_zone = "${data.google_dns_managed_zone.default.name}"
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
+}
+
+# Wildcard A record.
+resource "google_dns_record_set" "a_wildcard" {
+  name         = "*.${var.domain_name}."
+  managed_zone = "${data.google_dns_managed_zone.default.name}"
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
+}
+
+/*
+ * Outputs.
+ */
+
+output "load_balancer_ip" {
+  description = "IP address of the load balancer"
+  value = "${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"
+}
